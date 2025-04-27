2dc7db89cbef89a27ba78b69094c6fd00ded8d31
kurumi-program <kurumi.program@gmail.com>
2025-04-27 22:38:16 +0900

    cloudinary設定追加

--- config/initializers/cloudinary.rb
+++
@@ -1,6 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']  # Cloudinaryのcloud_name
-  config.api_key = ENV['CLOUDINARY_API_KEY']      # CloudinaryのAPIキー
-  config.api_secret = ENV['CLOUDINARY_API_SECRET'] # CloudinaryのAPIシークレット
-  config.secure = true
-end
