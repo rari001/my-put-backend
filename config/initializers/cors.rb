5fde8e37873cab340bc2053fdf82fc6900c4e34b
kurumi-program <kurumi.program@gmail.com>
2025-04-28 21:49:54 +0900

    corsの設定追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://techput.onrender.com"
+    origins "https://tech-put.com"
 
     resource "*",
       headers: :any,
