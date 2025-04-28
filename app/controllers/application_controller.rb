92eba3e995175c00c479900e0d37d1272ab57310
kurumi-program <kurumi.program@gmail.com>
2025-04-28 21:59:38 +0900

    application_controller設定追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-  before_action :redirect_if_using_default_domain
-
-  private
-
-  def redirect_if_using_default_domain
-    if request.host == "techput.onrender.com"
-      render json: { message: "Redirecting to new domain" }, status: 301, location: "https://tech-put.com#{request.fullpath}"
-    end
-  end
 end
